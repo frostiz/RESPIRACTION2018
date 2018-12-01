d1bfc5b8cdf343609f94105551215b0432a12d79
Olivier <olivier.de-la-sayette@epitech.eu>
2018-12-01 16:33:59 +0100

    before merge

--- Routes.js
+++ Routes.js
@@ -5,27 +5,28 @@ import Home from "./src/vues/Home";
 //import Pharma from "./src/vues/pharma/Pharma";
 //import Video from "./src/vues/patient/Video";
 //import Medicine from "./src/vues/patient/Medicine";
-//import Check from "./src/vues/patient/Check";
-//import Yes from "./src/vues/patient/Yes";
-//import No from "./src/vues/patient/No";
+import Check from "./src/vues/patient/Check";
+import Yes from "./src/vues/patient/Yes";
+import No from "./src/vues/patient/No";
 //import Map from "./src/vues/patient/Map";
 
 
 const Routes = () => (
     <Router>
         <Scene key="root">
-            <Scene key="home" component={Home} title="Home page" initial={true} />
+            <Scene key="pa_check" component={Check} title="Check page" initial={true} />
+            <Scene key="pa_yes" component={Yes} title = "Yes page" />
+            <Scene key="pa_no" component={No} title = "No page" />
         </Scene>
     </Router>
 );
 
 /*<Scene key="pa-patient" component={Patient} title="Patient One" />
-<Scene key="ph-pharma" component={Pharma} title="Pharma Two" />
-<Scene key="pa-video" component={Video} title = "Video page"/>
-<Scene key="pa-medicine" component={Medicine} title = "Medicine page" />
-<Scene key="pa-check" component={Check} title = "Check page" />
-<Scene key="pa-yes" component={Yes} title = "Yes page" />
-<Scene key="pa-no" component={No} title = "No page" />
-<Scene key="pa-map" component={Map} title = "Map page" />*/
+<Scene key="ph_pharma" component={Pharma} title="Pharma Two" />
+<Scene key="pa_video" component={Video} title = "Video page"/>
+<Scene key="pa_medicine" component={Medicine} title = "Medicine page" />
+<Scene key="pa_check" component={Check} title = "Check page" />
+<Scene key="pa_yes" component={Yes} title = "Yes.js page" />
+<Scene key="pa_map" component={Map} title = "Map page" />*/
 
 export default Routes;
--- src/vues/Home.js
+++ src/vues/Home.js
@@ -1,42 +1,13 @@
-import * as React from "react";
-import { Dimensions } from "react-native";
-import { TabView, TabBar, SceneMap } from "react-native-tab-view";
-import One from './patient/One';
-import Two from './pharma/Two';
+import React, { Component } from "react";
+import { AppRegistry, View } from "react-native";
+import Routes from "../../Routes.js";
 
 
 
-export default class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            index: 0,
-            routes: [
-                { key: "one", title: "page one" },
-                { key: "two", title: "page two" },
-            ]
-        };
-    }
-
-    componentDidMount() {
-    }
+class App extends Component {
     render() {
-        const OneRoute = () => (
-            <One />
-        );
-        const TwoRoute = () => (
-            <Two />
-        );
-        return (
-            <TabView
-                navigationState={this.state}
-                renderScene={SceneMap({
-                    one: OneRoute,
-                    two: TwoRoute,
-                })}
-                onIndexChange={index => this.setState({ index })}
-                initialLayout={{ width: Dimensions.get("window").width, height: Dimensions.get("window").height }}
-            />
-        );
+        return <Routes />;
     }
 }
+export default App;
+AppRegistry.registerComponent("app", () => App);
