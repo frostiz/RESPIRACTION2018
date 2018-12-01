206ffbc2829ce0f3caa6cc0517c4c5ac3d6aa7cd
frostiz <thibaut.trouve@epitech.eu>
2018-12-01 19:04:49 +0100

    Prepare for questions page

---
+++ src/vues/patient/End.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Actions } from 'react-native-router-flux';
+
+export default class Questions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render () {
+        return (
+            <View style={style.container}>
+                <Text>Merci d'avoir utiliser Parlon'Asthme !</Text>
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
---
+++ src/vues/patient/Prepare.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Actions } from 'react-native-router-flux';
+
+export default class Prepare extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    yes () {
+        Actions.pa_questions();
+    }
+    no () {
+        Actions.end();
+    }
+    render () {
+        return (
+            <View style={style.container}>
+                <Text>Pour préparer au mieux ton entretien, souhaites tu répondres à un petit questionnaire ?</Text>
+                <Button title='Oui' onPress={() => this.yes()} />
+                <Button title='Non' onPress={() => this.no()} />
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
