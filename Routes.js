99a5cfa53fe0bafde7f7d53fde2d9d9a8a629fca
frostiz <thibaut.trouve@epitech.eu>
2018-12-01 19:53:11 +0100

    merge

--- Routes.js
+++ Routes.js
@@ -18,15 +18,15 @@ const Routes = () => (
         <Scene key="root">
             <Scene key="home" component={Home} title="Home page" hideNavBar={true} initial={true} />
             <Scene key="pa_check" component={Check} title="Check page" />
-            <Scene key="pa_yes" component={Yes} title = "Location page" hideNavBar={true} />
-            <Scene key="pa_no" component={No} title = "No page" hideNavBar={true} />
+            <Scene key="pa_yes" component={Yes} title = "Location page"/>
+            <Scene key="pa_no" component={No} title = "No page"/>
             <Scene key="pa_home" component={PatientHome} title="Patient home page" hideNavBar={true} />
-            <Scene key="pa_scan" component={PatientScan} title="Patient scan medics page" hideNavBar={false} />
-            <Scene key="pa_form" component={PatientForm} title="Patient form" hideNavBar={false} />
+            <Scene key="pa_scan" component={PatientScan} title="Patient scan medics page" />
+            <Scene key="pa_form" component={PatientForm} title="Patient form"/>
             <Scene key="pa_prepare" component={PatientPrepare} title={"Patient prepare"} />
             <Scene key="pa_questions" component={PatientQuestions} title={"Patient questions"} />
             <Scene key="pa_end" component={PatientEnd} title={"Patient end"} />
-            <Scene key="ph_home" component={PharmaHome} title="Pharma home page" hideNavBar={false} />
+            <Scene key="ph_home" component={PharmaHome} title="Pharma home page"/>
             <Scene key="pa_map" component={Map} title = "Map page"/>
         </Scene>
     </Router>
