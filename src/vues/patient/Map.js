7606968df150e366eb9c326fea5b67fcaf28fb0d
Olivier <olivier.de-la-sayette@epitech.eu>
2018-12-01 21:43:05 +0100

    merge

--- src/vues/patient/Map.js
+++ src/vues/patient/Map.js
@@ -16,15 +16,24 @@ export default class Two extends React.Component {
         return (
             <View style={{ flex: 1, backgroundColor: '#00A1AB', }}>
                 <View style={{justifyContent: 'center', alignItems: 'center', paddingTop: 50}}>
-                    <Text style={{color:"white"}}> Votre localisation </Text>
+                    <Text style={{color:"white"}}> Pharmacie la plus proche </Text>
                 </View>
                 <View>
                     <Image resizeMode="contain"
-                           style={{width: Dimensions.get("window").width, height: Dimensions.get("window").height}}
-                           source={{uri: 'https://www.google.com/search?q=google+map&client=firefox-b-ab&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5q8rqmf_eAhUN-YUKHdZHBDEQ_AUIECgD&biw=1920&bih=915#imgrc=UEqmFZHK4PSGYM:'}}/>
+                           style={{width: Dimensions.get("window").width, height: Dimensions.get("window").height / 2}}
+                           source={{uri: 'http://img.20mn.fr/lYrLCO3kTEi5ot_CZWPAuA/561x360_localisation-rue-stations-lille'}}/>
                 </View>
-                <View style={style.bottomButton}>
-                    <Button title='Valider' onPress={() => this.goToPage()}/>
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{color:"white", borderWidth: 1, borderColor: "white", marginTop: -50, marginBottom: 20, textAlign: 'center'}}> Pharmacie De la croix {"\n"} 3 rue Alexandre Dumas {"\n"} 59000 Lille </Text>
+                </View>
+                <View style={style.reservationOneButton}>
+                    <Button title='Samedi 8 / 12 à 10h30' onPress={() => this.goToPage()}/>
+                </View>
+                <View style={style.reservationButton}>
+                    <Button title='Lundi 10 / 12 à 11h' onPress={() => this.goToPage()}/>
+                </View>
+                <View style={style.reservationButton}>
+                    <Button title='Jeudi 13 / 12 à 15h' onPress={() => this.goToPage()}/>
                 </View>
             </View>
         )
@@ -32,11 +41,17 @@ export default class Two extends React.Component {
 }
 
 const style = StyleSheet.create({
-    bottomButton: {
+    reservationButton: {
         width: '100%',
         paddingLeft: 20,
         paddingRight: 20,
-        position: 'absolute',
-        bottom: 10
+        paddingBottom: 20
+    },
+    reservationOneButton: {
+        width: '100%',
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        //paddingTop: 100
     }
 });
