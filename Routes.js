cf056bd7f8e72848fadcb69b884bea55d6083ef3
Olivier <olivier.de-la-sayette@epitech.eu>
2018-12-01 18:33:30 +0100

    merge

--- Routes.js
+++ Routes.js
@@ -7,18 +7,19 @@ import PharmaHome from './src/vues/pharma/Home';
 import Check from "./src/vues/patient/Check";
 import Yes from "./src/vues/patient/Yes";
 import No from "./src/vues/patient/No";
-//import Map from "./src/vues/patient/Map";
+import Map from "./src/vues/patient/Map";
 
 const Routes = () => (
     <Router>
         <Scene key="root">
             <Scene key="pa_check" component={Check} title="Check page" initial={true} />
-            <Scene key="pa_yes" component={Yes} title = "Yes page" hideNavBar={true} />
+            <Scene key="pa_yes" component={Yes} title = "Localisation page"/>
             <Scene key="pa_no" component={No} title = "No page" hideNavBar={true} />
             <Scene key="home" component={Home} title="Home page" hideNavBar={true} initial={true} />
             <Scene key="pa_home" component={PatientHome} title="Patient home page" hideNavBar={true} />
             <Scene key="pa_scan" component={PatientScan} title="Patient scan medics page" hideNavBar={false} />
             <Scene key="ph_home" component={PharmaHome} title="Pharma home page" hideNavBar={false} />
+            <Scene key="pa_map" component={Map} title = "Map page"/>
         </Scene>
     </Router>
 );
--- src/vues/patient/Yes.js
+++ src/vues/patient/Yes.js
@@ -1,20 +1,54 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-
-
+import {Button, Platform, StyleSheet, Text, View} from "react-native";
+import { TextField } from 'react-native-material-textfield';
+import RaisedTextButton from "react-native-material-buttons/src/components/raised-text-button";
+import { Actions } from 'react-native-router-flux';
 
 export default class Two extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            inputCity:''
         };
+    }
+    goToPage() {
+        Actions.pa_map();
     }
     render () {
         return (
-            <View>
-                <Text> Vous êtes éligible </Text>
-
+            <View style={style.container} >
+                <View style={{justifyContent: 'center', alignItems: 'center', paddingTop: 50}}>
+                    <Text> Vous êtes éligible </Text>
+                </View>
+                <View style={style.medicField}>
+                    <TextField label="Entrez votre ville" value={this.state.inputCity} onChangeText={(input) => this.setState({inputCity: input})} />
+                </View>
+                <View style={style.medicButton}>
+                    <RaisedTextButton onPress={() => this.goToPage()} title='Géolocalise moi' color={TextField.defaultProps.tintColor} titleColor='white' />
+                </View>
+            <View style={style.bottomButton}>
+                    <Button title='Valider' onPress={() => this.goToPage()}/>
+                </View>
             </View>
         )
     }
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    medicField: {
+        padding: 20,
+    },
+    medicButton: {
+        padding: 50,
+    },
+    bottomButton: {
+        width: '100%',
+        paddingLeft: 20,
+        paddingRight: 20,
+        position: 'absolute',
+        bottom: 10
+    }
+});
