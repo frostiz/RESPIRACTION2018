782dcd84c305db6ca5cd29284dce933610a9935b
Olivier <olivier.de-la-sayette@epitech.eu>
2018-12-01 19:05:16 +0100

    merge

--- src/vues/patient/Map.js
+++ src/vues/patient/Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-
+import {Text, View, Image, Dimensions, Button, StyleSheet} from "react-native";
+import { Actions } from 'react-native-router-flux';
 
 
 export default class Two extends React.Component {
@@ -9,11 +9,34 @@ export default class Two extends React.Component {
         this.state = {
         };
     }
+    goToPage() {
+        Actions.pa_form();
+    }
     render () {
         return (
-            <View>
-                <Text> Map </Text>
+            <View style={{ flex: 1 }}>
+                <View>
+                    <Text> Votre localisation </Text>
+                </View>
+                <View>
+                    <Image resizeMode="contain"
+                           style={{width: Dimensions.get("window").width, height: Dimensions.get("window").height}}
+                           source={{uri: 'https://www.google.com/search?q=google+map&client=firefox-b-ab&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5q8rqmf_eAhUN-YUKHdZHBDEQ_AUIECgD&biw=1920&bih=915#imgrc=UEqmFZHK4PSGYM:'}}/>
+                </View>
+                <View style={style.bottomButton}>
+                    <Button title='Valider' onPress={() => this.goToPage()}/>
+                </View>
             </View>
         )
     }
 }
+
+const style = StyleSheet.create({
+    bottomButton: {
+        width: '100%',
+        paddingLeft: 20,
+        paddingRight: 20,
+        position: 'absolute',
+        bottom: 10
+    }
+});
